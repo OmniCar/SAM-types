f6dfe82c164224637d55b2996661ce30f2e97aee
Claus Stilborg <cs@omnicar.dk>
2018-05-02 17:07:03 +0200

    Adds types for looking up and returning CVR information from an external service (#12)

---
+++ types/vatLookup.d.ts
@@ -0,0 +1,14 @@
+export interface IVatLookupRequest {
+  cvrCode: string,
+  country: string
+}
+
+export interface IVatLookupResponse {
+  cvrCode: string,
+  country: string,
+  administrativeName: string,
+  address1: string,
+  postal_code: string,
+  city: string,
+  address2: string
+}
