b20f77185e10b1f394481299c457b2de9a3d552c
IgorJoergensen <ij@omnicar.dk>
2018-05-14 13:09:16 +0200

    removed the definitions for businessVatId and businessTaxId

--- types/stripe/stripeLegalEntity.d.ts
+++ types/stripe/stripeLegalEntity.d.ts
@@ -5,8 +5,6 @@ import { IStripeDateOfBirth } from './stripeDateOfBirth'
 
 export interface IStripeLegalEntity {
   type: StripeEntityType // Either "individual" or "company", for what kind of legal entity the account owner is for
-  businessTaxId: string
-  businessVatId: string
   dob: IStripeDateOfBirth // Primary owner
   firstName: string
   lastName: string
