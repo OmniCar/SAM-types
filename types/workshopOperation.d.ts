69dd8fdac7f924f80ed777a65b4b103fc46bb84e
MLanggaard <31535614+MLanggaard@users.noreply.github.com>
2018-05-29 14:56:21 +0200

    Workshop operation (#24)

---
+++ types/workshopOperation.d.ts
@@ -0,0 +1,11 @@
+import { PriceSpecification } from './index'
+
+export type WorkshopOrderBy = 'id' | 'date'
+
+export interface IWorkshopOperation {
+  id?: number
+  price: PriceSpecification
+  mileage: number
+  date: Date
+  description: string
+}
