5033b66f37fe6e85220b6731548e511c76163240
Ole Frank Jensen <olefrankjensen@gmail.com>
2018-05-29 11:13:17 +0200

    add missing file

---
+++ types/includedContractOption.d.ts
@@ -0,0 +1,12 @@
+import { PriceSpecification } from './priceSpecification'
+import { IContractResponse } from './contract/contract'
+
+export interface IIncludedContractOption {
+  id: number
+  optionId: number
+  description: string
+  conditionText: string
+  weight: number
+  price: PriceSpecification
+  assoc: 'PROPERTY' | 'ADDITIONAL'
+}
