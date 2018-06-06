1092da7431fb2b271bcae39ccf5662d0425aa04f
IgorJoergensen <ij@omnicar.dk>
2018-06-06 13:03:27 +0000

    removed redundant emailexists type and login deals with all, also getting ready for user metadata

--- types/user.d.ts
+++ types/user.d.ts
@@ -1,7 +1,15 @@
 import { IContactPersonResponse } from './contactPerson'
 
 export interface IUserResponse {
-    userId: number
-    email: string
-    contactPerson: IContactPersonResponse
+  userId: number
+  email: string
+  contactPerson: IContactPersonResponse
+}
+
+export interface IUserTheme {
+  name: string
+}
+
+export interface IUserMetadata {
+  theme: IUserTheme
 }
