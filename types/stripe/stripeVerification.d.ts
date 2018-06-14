a9c17bdc1c0635ded0b91ce26d73f64d27839a50
IgorJoergensen <ij@omnicar.dk>
2018-06-14 12:36:35 +0200

    types removed and replaced with enums

--- types/stripe/stripeVerification.d.ts
+++ types/stripe/stripeVerification.d.ts
@@ -1,13 +1,3 @@
-export type StripeVerificationReason =
-  | 'rejected.fraud'
-  | 'rejected.terms_of_service'
-  | 'rejected.listed'
-  | 'rejected.other'
-  | 'fields_needed'
-  | 'listed'
-  | 'under_review'
-  | 'other'
-
 export enum StripeVerificationReasonEnum {
   'rejected.fraud' = 'Rejected on suspicion of fraud',
   'rejected.terms_of_service' = 'Rejected due to Terms of Service violation',
@@ -18,24 +8,6 @@ export enum StripeVerificationReasonEnum {
   'under_review' = 'Under Review',
   'other' = 'Other',
 }
-
-export type StripeVerificationField =
-  | 'business_url'
-  | 'external_account'
-  | 'legal_entity.address.city'
-  | 'legal_entity.address.line1'
-  | 'legal_entity.address.postal_code'
-  | 'legal_entity.address.state'
-  | 'legal_entity.dob.day'
-  | 'legal_entity.dob.month'
-  | 'legal_entity.dob.year'
-  | 'legal_entity.first_name'
-  | 'legal_entity.last_name'
-  | 'legal_entity.type'
-  | 'product_description'
-  | 'support_phone'
-  | 'tos_acceptance.date'
-  | 'tos_acceptance.ip'
 
 export enum StripeVerificationFieldEnum {
   'business_url' = 'Business URL is missing',
@@ -57,7 +29,7 @@ export enum StripeVerificationFieldEnum {
 }
 
 export interface IStripeVerification {
-  disabled_reason: StripeVerificationReason | null
+  disabled_reason: StripeVerificationReasonEnum | null
   due_by: string | null
-  fields_needed: StripeVerificationField[]
+  fields_needed: StripeVerificationFieldEnum[]
 }
