9d17b099b7db5d136879cdd2a42d2544d9a4d549
IgorJoergensen <ij@omnicar.dk>
2018-06-20 11:20:42 +0200

    removed enums and replaced with types

--- types/index.ts
+++ types/index.ts
@@ -33,36 +33,6 @@ export enum ContractActionType {
   settle = 900, // Who Settled the contract, any settlement comments will be in the details
 }
 
-export enum StripeVerificationReasonEnum {
-  'rejected.fraud' = 'Rejected on suspicion of fraud',
-  'rejected.terms_of_service' = 'Rejected due to Terms of Service violation',
-  'rejected.listed' = 'Rejected due to reason listed',
-  'rejected.other' = 'Rejected for other reason',
-  'fields_needed' = 'Fields are needed',
-  'listed' = 'Listed',
-  'under_review' = 'Under Review',
-  'other' = 'Other',
-}
-
-export enum StripeVerificationFieldEnum {
-  'business_url' = 'Business URL is missing',
-  'external_account' = 'No External Accounts created',
-  'legal_entity.address.city' = 'City in address for Legal Entity is missing',
-  'legal_entity.address.line1' = 'First line in address for Legal Entity is missing',
-  'legal_entity.address.postal_code' = 'Postal code in address for Legal Entity is missing',
-  'legal_entity.address.state' = 'State in address for Legal Entity is missing',
-  'legal_entity.dob.day' = 'Day in Date of Birth for Legal Entity is missing',
-  'legal_entity.dob.month' = 'Month in Date of Birth for Legal Entity is missing',
-  'legal_entity.dob.year' = 'Year in Date of Birth for Legal Entity is missing',
-  'legal_entity.first_name' = 'First Name for Legal Entity is missing',
-  'legal_entity.last_name' = 'Last Name for Legal Entity is missing',
-  'legal_entity.type' = 'Type for Legal Entity is missing',
-  'product_description' = 'Product Description is missing',
-  'support_phone' = 'Support Phone number is missing',
-  'tos_acceptance.date' = 'Terms of Service Acceptance Date is missing',
-  'tos_acceptance.ip' = 'Terms of Service Acceptance IP is missing',
-}
-
 export { IAddressRequest, IAddressResponse } from './address'
 export { App } from './app'
 export { Brand } from './brand'
@@ -90,7 +60,13 @@ export { PaymentCard } from './paymentCard'
 export { PaymentPlan } from './paymentPlan'
 export { ICalculationRequest, ICalculationResponse } from './priceCalculation'
 export { PriceSpecification } from './priceSpecification'
-export { IBrandingPayloadResponse, IStripeInformationResponse, IProviderInformationResponse, IProviderBrandingResponse, IProviderStylingResponse } from './providerBranding'
+export {
+  IBrandingPayloadResponse,
+  IStripeInformationResponse,
+  IProviderInformationResponse,
+  IProviderBrandingResponse,
+  IProviderStylingResponse,
+} from './providerBranding'
 export { CalculatorResponseErrors } from './responseErrors/calculatorResponseErrors'
 export { ContractResponseErrors } from './responseErrors/contractResponseErrors'
 export { GenericResponseErrors } from './responseErrors/genericResponseTypes'
@@ -107,7 +83,11 @@ export { IStripeLegalEntity } from './stripe/stripeLegalEntity'
 export { IStripeOwner } from './stripe/stripeOwner'
 export { ITermsOfServiceRequest, ITermsOfServiceResponse } from './termsOfService'
 export { Translation } from './translation'
-export { ITableUserConfiguration, IColumnConfiguration, UserConfigurationType } from './userConfiguration/tableUserConfiguration'
+export {
+  ITableUserConfiguration,
+  IColumnConfiguration,
+  UserConfigurationType,
+} from './userConfiguration/tableUserConfiguration'
 export { Vehicle } from './vehicle'
 export { VehicleContracts } from './vehicleContracts'
 export { ILoginRequest, ILoginResponse } from './login'
--- types/stripe/stripeVerification.d.ts
+++ types/stripe/stripeVerification.d.ts
@@ -1,7 +1,33 @@
-import { StripeVerificationReasonEnum, StripeVerificationFieldEnum } from '../index'
+export type StripeVerificationReason =
+  | 'rejected.fraud'
+  | 'rejected.terms_of_service'
+  | 'rejected.listed'
+  | 'rejected.other'
+  | 'fields_needed'
+  | 'listed'
+  | 'under_review'
+  | 'other'
+
+export type StripeVerificationField =
+  | 'business_url'
+  | 'external_account'
+  | 'legal_entity.address.city'
+  | 'legal_entity.address.line1'
+  | 'legal_entity.address.postal_code'
+  | 'legal_entity.address.state'
+  | 'legal_entity.dob.day'
+  | 'legal_entity.dob.month'
+  | 'legal_entity.dob.year'
+  | 'legal_entity.first_name'
+  | 'legal_entity.last_name'
+  | 'legal_entity.type'
+  | 'product_description'
+  | 'support_phone'
+  | 'tos_acceptance.date'
+  | 'tos_acceptance.ip'
 
 export interface IStripeVerification {
-  disabled_reason: StripeVerificationReasonEnum | null
+  disabled_reason: StripeVerificationReason | null
   due_by: string | null
-  fields_needed: StripeVerificationFieldEnum[]
+  fields_needed: StripeVerificationField[]
 }
