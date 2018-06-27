a0e5bf7791f05de221dfb7a6319d055fa7cea1e9
IgorJoergensen <ij@omnicar.dk>
2018-06-27 15:22:30 +0200

    extended with creation and expiration, and added an interface to handle those

--- types/contract/contractFilter.d.ts
+++ types/contract/contractFilter.d.ts
@@ -1,7 +1,14 @@
+export interface IContractFilterDate {
+  start: Date
+  end: Date
+}
+
 export interface IContractFilterOptions {
   contractStates: IContractFilterOption[]
   templates: IContractFilterOption[]
   sellers: IContractFilterOption[]
+  created: IContractFilterDate
+  expired: IContractFilterDate
 }
 
 export interface IContractFilterOption {
