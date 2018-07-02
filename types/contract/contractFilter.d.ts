a0cc295164ad01f06d0f3e8a32d88d034e37d68f
IgorJoergensen <ij@omnicar.dk>
2018-07-02 14:20:11 +0200

    synched with ole's PR

--- types/contract/contractFilter.d.ts
+++ types/contract/contractFilter.d.ts
@@ -1,18 +1,18 @@
 export interface IContractFilterDate {
-  start: string
-  end: string
-}
-
-export interface IContractFilterOptions {
-  contractStates?: IContractFilterOption[]
-  templates?: IContractFilterOption[]
-  options?: IContractFilterOption[]
-  sellers?: IContractFilterOption[]
-  creation?: IContractFilterDate
-  expiration?: IContractFilterDate
+  min: string
+  max: string
 }
 
 export interface IContractFilterOption {
   title: string
   id: number
+}
+
+export interface IContractFilterOptions {
+  contractStates: IContractFilterOption[]
+  templates: IContractFilterOption[]
+  options: IContractFilterOption[]
+  sellers: IContractFilterOption[]
+  creation: IContractFilterDate
+  expiration: IContractFilterDate
 }
