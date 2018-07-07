79655400fcf37b6031d78a7ebdb30861eaaf7937
IgorJoergensen <ij@omnicar.dk>
2018-07-08 00:21:47 +0200

    all exports now asterix and type for all userconfigs made

--- types/userConfiguration/userConfiguration.d.ts
+++ types/userConfiguration/userConfiguration.d.ts
@@ -1,4 +1,5 @@
 import { TableConfigurationType, ITableUserConfiguration } from './tableUserConfiguration.d'
 
 export type UserConfigurationType = TableConfigurationType
-export type UserConfiguration = ITableUserConfiguration
+export type UserConfiguration<OrderBy> = ITableUserConfiguration<OrderBy>
+export type UserConfigurations<OrderBy> = Record<string, ITableUserConfiguration<OrderBy>>
