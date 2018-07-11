6464f4b740530be2365103c1b95bcce5cce7bfb0
IgorJoergensen <ij@omnicar.dk>
2018-07-11 20:26:54 +0200

    needed interface for contract filtering as params

--- types/contract/contractFilter.d.ts
+++ types/contract/contractFilter.d.ts
@@ -8,12 +8,20 @@ export interface IContractFilterOption {
   id: number
 }
 
-// @NOTE - Deprecate this
 export interface IContractFilterOptions {
   contractStates: IContractFilterOption[]
   templates: IContractFilterOption[]
   options: IContractFilterOption[]
   sellers: IContractFilterOption[]
+  created: IContractFilterDate
+  expired: IContractFilterDate
+}
+
+export interface IContractFilterParams {
+  contractStates: number[]
+  templates: number[]
+  options: number[]
+  sellers: number[]
   created: IContractFilterDate
   expired: IContractFilterDate
 }
