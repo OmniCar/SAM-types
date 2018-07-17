a9d882ca4f8d3859a60bf9f6ebd05d5bf803864a
Erik <bager.erik@gmail.com>
2018-07-17 13:21:11 +0200

    Add short identifier and urls to details

--- types/contract/contractDetails.d.ts
+++ types/contract/contractDetails.d.ts
@@ -1,9 +1,9 @@
-import { IQueryParams } from './../queryParams';
+import { IQueryParams } from './../queryParams'
 import { PriceSpecification } from '../priceSpecification'
 import { IContactPersonResponse } from '../contactPerson'
 import { IContractResponse } from './contract'
 import { IContractOptionResponse } from '../contractOption'
-import { IIncludedContractOption } from '../includedContractOption';
+import { IIncludedContractOption } from '../includedContractOption'
 
 export interface IContractDetailsRecord {
   type: string
@@ -18,4 +18,7 @@ export interface IContractDetailsRecord {
   endMileage: number
   extendedFrom: IContractResponse | null
   includedOptions: IIncludedContractOption[] | null
+  prettyIdentifierShort: string
+  termsPdfUrl: string
+  contractPdfUrl: string
 }
