e9c3a387057402cbe85964ab20603633bfd26a8c
Richard <rbanyi@me.com>
2018-07-20 13:05:46 +0200

    removed prefix in ICustomerListRecord

--- types/admin/customer/customerList.d.ts
+++ types/admin/customer/customerList.d.ts
@@ -1,10 +1,10 @@
 export type CustomerListOrderByType = 'name' | 'address' | 'zip' | 'city' | 'phone' | 'email'
 
 export interface ICustomerListRecord {
-  customerName: string
-  customerEmail: string
-  customerPhone: string
-  customerAddress: string
-  customerCity: string
-  customerZip: string | number
+  name: string
+  email: string
+  phone: string
+  address: string
+  city: string
+  zip: string | number
 }
