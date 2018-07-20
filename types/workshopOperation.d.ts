e3a702a47c4647871366f46b330e96eda8a614a6
IgorJoergensen <ij@omnicar.dk>
2018-07-20 12:48:57 +0200

    Split workshopoperation interface into two - one for list and one for details

--- types/workshopOperation.d.ts
+++ types/workshopOperation.d.ts
@@ -2,10 +2,22 @@ import { PriceSpecification } from './index'
 
 export type WorkshopOrderBy = 'id' | 'date'
 
-export interface IWorkshopOperation {
+export interface IWorkshopOperationListRecord {
   id?: number
   price: PriceSpecification
   mileage: number
   date: Date | string
   description: string
+}
+
+export interface IWorkshopOperationDetailsRecord {
+  id: number
+  date: Date | string
+  price: PriceSpecification
+  mileage: string
+  description: string
+  userId: string
+  userName: string
+  vehicleId: string
+  contractId: string
 }
