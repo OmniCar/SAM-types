94bad78949e6cf8764bb21e1aa051201e9aa80da
Richard Banyi <rbanyi@me.com>
2018-08-15 13:34:22 +0200

    Feature/admin users type (#81)
    * UserListOrderByType
    
    * UserListOrderByType
    
    * added user_id into userListRecord;
    
    * camelCase userId

---
+++ types/admin/user/userList.d.ts
@@ -0,0 +1,9 @@
+export type UserListOrderByType = 'name' | 'role' | 'email' | 'phone'
+
+export interface IUserListRecord {
+  userId: number
+  name: string
+  email: string
+  phone: string
+  role: string
+}
