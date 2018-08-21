d16323c8e2766bb8247ddabd73d3c52ce925096c
Martin Kock <mk@omnicar.dk>
2018-08-21 15:29:15 +0200

    use predefined Vehicle type

--- types/admin/contractCreation/contractCreation.d.ts
+++ types/admin/contractCreation/contractCreation.d.ts
@@ -1,6 +1,7 @@
 import { IUserInfo } from 'admin/user/user'
 import { IContractTemplateResponse } from '../../contractTemplate'
 import { PaymentGateway } from '../../payment'
+import { Vehicle } from '../../vehicle'
 import { ICarCollection } from './carData'
 import { ContractType } from './priceCalculation'
 
@@ -20,6 +21,7 @@ interface ICommonContractCreationRequest {
   contractTemplateId: number
   duration: number
   mileage: number
+  vehicle: Vehicle
   additionalOptionIds: number[]
   providerPayments: number
   providerShare: number
@@ -32,24 +34,10 @@ interface ICommonContractCreationRequest {
 export interface ICustomContractCreationRequest extends ICommonContractCreationRequest {
   type: 'CUSTOM'
   amountPerPayment: number
-  vehicle: {
-    dateVehicleFirstRegistered: Date | string
-    vin: string
-    registrationNumber: string
-    brandName: string
-    modelName: string
-    fuelTypeName: string
-  }
 }
 
 export interface IStandardContractCreationRequest extends ICommonContractCreationRequest {
   type: 'STANDARD'
-  vehicle: {
-    modelId?: number
-    dateVehicleFirstRegistered: Date | string
-    vin: string
-    registrationNumber: string
-  }
 }
 
 // Note: there is currently no need for a IContractCreationResponse, just a 200 OK is sufficient.
