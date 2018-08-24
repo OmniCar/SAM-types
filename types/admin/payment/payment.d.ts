ce16fc08b5139714b6e494b4a04103eb0ef71a55
Erik <bager.erik@gmail.com>
2018-08-24 08:20:13 +0000

    Add ICreditCardRecord

--- types/admin/payment/payment.d.ts
+++ types/admin/payment/payment.d.ts
@@ -3,3 +3,12 @@ export interface ICreditCard {
   cardId: string
   isDefault: boolean
 }
+
+export interface ICreditCardRecord {
+  cardId: string
+  brand: string
+  expireYear: number
+  expireMonth: number
+  last4: string
+  isDefault: boolean
+}
