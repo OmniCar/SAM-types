94a67cbac0479593eefeb7b54989dcfe483e39b5
IgorJoergensen <ij@omnicar.dk>
2018-08-27 14:37:58 +0200

    further split up

--- types/durationOptions.d.ts
+++ types/durationOptions.d.ts
@@ -1,11 +1,13 @@
 import { PriceSpecification } from './priceSpecification'
 
+export interface DurationOptionsOptionsPrices {
+  contractId: number
+  price: PriceSpecification
+}
+
 export interface DurationOptionsOptions {
   mileage: number
-  prices: {
-    contractId: number
-    price: PriceSpecification
-  }[]
+  prices: DurationOptionsOptionsPrices[]
 }
 
 export interface DurationOptions {
