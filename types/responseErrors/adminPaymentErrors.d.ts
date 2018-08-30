b3e1b2d5b7d64a317254ad8c08b5158d525eb4a3
erikoc <32451595+erikoc@users.noreply.github.com>
2018-08-30 13:53:00 +0200

    Add generic payment errors (#98)

--- types/responseErrors/adminPaymentErrors.d.ts
+++ types/responseErrors/adminPaymentErrors.d.ts
@@ -1,1 +1,4 @@
-export type AdminPaymentErrors = 'DEFAULT_CARD_CANNOT_BE_DELETED_ERROR'
+export type AdminPaymentErrors =
+  | 'DEFAULT_CARD_CANNOT_BE_DELETED_ERROR'
+  | 'INVALID_CARD_TOKEN_ERROR'
+  | 'INVALID_CARD_ERROR'
