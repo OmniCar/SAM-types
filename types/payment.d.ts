3f31d86191b63eff511f4e5fcc370617b5b26764
Martin Kock <mk@omnicar.dk>
2018-09-14 13:08:31 +0200

    Define contract activation types

--- types/payment.d.ts
+++ types/payment.d.ts
@@ -3,6 +3,9 @@ import { PriceSpecification } from './priceSpecification'
 /** supported payment gateways */
 export type PaymentGateway = 'Stripe' | 'B2B'
 
+/** supported payment types */
+export type PaymentType = 'automatic' | 'manual'
+
 /** credit card */
 export interface PaymentCard {
   cardType: string
