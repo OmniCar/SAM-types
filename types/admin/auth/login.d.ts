53c7aa968a64c6cdee00af2337dc784b97c78492
Erik <bager.erik@gmail.com>
2018-10-10 12:17:12 +0200

    Add IAdminCustomer to ILoginResponse

--- types/admin/auth/login.d.ts
+++ types/admin/auth/login.d.ts
@@ -2,6 +2,7 @@ import { UserRole } from './roles'
 import { IAdminUserInfo } from '../user/user'
 import { IContractProviderResponse } from '../../contractProvider'
 import { IContractProvider } from '../contractProvider'
+import { IAdminCustomer } from '../customer';
 
 export interface ILoginRequest {
   username: string
@@ -14,6 +15,7 @@ export interface ILoginResponse {
   role?: UserRole
   token?: string
   userInfo?: IAdminUserInfo
+  customerInfo?: IAdminCustomer
   providerInfo?: IContractProvider
   stripePublicKey?: string
 }
