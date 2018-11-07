dfbd3f24311c69cff6b9bd2c51512ced4e5701c5
Richard <rbanyi@me.com>
2018-11-07 11:09:18 +0100

    orderBy contract creationDate;

--- types/contract/contractList.d.ts
+++ types/contract/contractList.d.ts
@@ -5,7 +5,7 @@ import { IContractHealth } from './../admin/contract/health'
 export interface IContractListRecord {
   prettyIdentifier: string
   brand: string
-  contractEndDate: Date | string
+  contractCreationDate: Date | string
   contractStartDate: Date | string
   contractState: ContractState
   contractTemplateName: string
@@ -18,7 +18,7 @@ export interface IContractListRecord {
 export type ContractListOrderByType =
   | 'prettyIdentifier'
   | 'brand'
-  | 'contractEndDate'
+  | 'contractCreationDate'
   | 'contractStartDate'
   | 'contractState'
   | 'contractTemplateName'
