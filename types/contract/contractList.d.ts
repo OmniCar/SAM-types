82d29f41a5b94fa390b9619f84c2cbecae6bbfc5
Richard <rbanyi@me.com>
2018-11-15 14:01:42 +0100

    oder by customer COmpany name

--- types/contract/contractList.d.ts
+++ types/contract/contractList.d.ts
@@ -22,6 +22,7 @@ export type ContractListOrderByType =
   | 'contractEndDate'
   | 'contractCreationDate'
   | 'contractState'
+  | 'customerCompanyName'
   | 'contractTemplateName'
   | 'customerName'
   | 'registrationNumber'
