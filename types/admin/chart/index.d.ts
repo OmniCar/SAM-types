cf21a71ee45faafea2400f739d1a9acc5ffc60a4
Martin Kock <mk@omnicar.dk>
2018-11-26 16:30:17 +0100

    Add types for economy chart

---
+++ types/admin/chart/index.d.ts
@@ -0,0 +1,1 @@
+export { IPaymentChart } from './contractCharts'
--- types/admin/index.d.ts
+++ types/admin/index.d.ts
@@ -1,4 +1,5 @@
 export * from './auth'
+export * from './chart'
 export * from './contract'
 export * from './contractCreation'
 export * from './customer'
