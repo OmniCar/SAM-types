8fe9771300d93a3994678f26d1ea3aa65022c3e6
erikoc <32451595+erikoc@users.noreply.github.com>
2018-11-29 13:01:10 +0100

    Add IWorkshopOperationCollection to group ops (#158)

--- types/workshopOperation.d.ts
+++ types/workshopOperation.d.ts
@@ -1,6 +1,11 @@
 import { PriceSpecification } from './index'
 
 export type WorkshopOrderBy = 'id' | 'date'
+export interface IWorkshopOperationCollection {
+  date: string
+  price: PriceSpecification
+  operations: IWorkshopOperation[]
+}
 
 export interface IWorkshopOperation {
   id?: number
