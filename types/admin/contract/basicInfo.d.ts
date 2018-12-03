550b049e1474e815d5b97d3ea2725b48d5281e36
Erik <bager.erik@gmail.com>
2018-12-03 09:39:40 +0100

    Add contractType to IBasicContractInfo

--- types/admin/contract/basicInfo.d.ts
+++ types/admin/contract/basicInfo.d.ts
@@ -1,8 +1,9 @@
-import { ContractState, IIncludedContractOption } from '../../index'
+import { ContractState, IIncludedContractOption, ContractType } from '../../index'
 import { IContractHealth } from './health'
 
 export interface IBasicContractInfo {
   contractState: ContractState
+  contractType: ContractType
   templateDescription: string
   additionalOptions: IIncludedContractOption[]
   health: IContractHealth[]
