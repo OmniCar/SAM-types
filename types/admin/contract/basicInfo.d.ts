98cca03fdfe05d37bc0f0f2c7b0aec0dba56d5d2
Martin Kock <mk@omnicar.dk>
2019-01-09 14:47:14 +0100

    Add payment gateway to IBasicContractInfo (#174)

--- types/admin/contract/basicInfo.d.ts
+++ types/admin/contract/basicInfo.d.ts
@@ -1,9 +1,10 @@
-import { ContractState, IIncludedContractOption, ContractType } from '../../index'
+import { ContractState, ContractType, IIncludedContractOption, PaymentGateway } from '../../index'
 import { IContractHealth } from './health'
 
 export interface IBasicContractInfo {
   contractState: ContractState
   contractType: ContractType
+  paymentGateway: PaymentGateway
   templateDescription: string
   additionalOptions: IIncludedContractOption[]
   health: IContractHealth[]
