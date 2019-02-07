0ceab143f9836e985e857760ce8c27d1b0effc81
Martin Kock <mk@omnicar.dk>
2019-02-07 09:55:26 +0100

    Custom terms

--- types/admin/contract/contractDetails.d.ts
+++ types/admin/contract/contractDetails.d.ts
@@ -1,10 +1,8 @@
-import { IQueryParams } from './../../queryParams'
-import { PriceSpecification } from './../../priceSpecification'
 import { IContactPersonResponse } from './../../contactPerson'
 import { IContractResponse } from './../../contract/contract'
-import { IContractOptionResponse } from './../../contractOption'
 import { IIncludedContractOption } from './../../includedContractOption'
 import { PaymentGateway } from './../../payment'
+import { PriceSpecification } from './../../priceSpecification'
 
 export interface IContractDetailsRecord {
   contractPdfUrl: string
@@ -26,4 +24,8 @@ export interface IContractDetailsRecord {
   termsPdfUrl: string
   totalPrice: PriceSpecification
   type: string
+}
+
+export interface IAdminCustomTermsRecord {
+  customTerms: string
 }
