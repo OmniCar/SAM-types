cba0442a140642ecad5ee9fabe75904197a91d0f
Claus Stilborg <cs@omnicar.dk>
2019-09-04 10:58:47 +0200

    Feature/adjust offerresponse for stripecheckout part deux (#227)
    * Roll back last change and add new type for payment prep
    
    * Roll back last change add new type for payment prep

---
+++ types/contract/contractPayment.d.ts
@@ -0,0 +1,8 @@
+import { PaymentGateway } from 'payment'
+
+export interface IPreparePaymentResponse {
+  contractId: number
+  paymentGateway: PaymentGateway
+  checkoutSessionId?: string
+  stripePublicKey?: string
+}
