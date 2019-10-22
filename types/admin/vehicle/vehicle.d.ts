cb06f2e9c97ae382bed82a218aa60fbbe42658f2
fragus-kurt <54097185+fragus-kurt@users.noreply.github.com>
2019-10-22 12:28:37 +0200

    Warranties - add missing field termsOfService to IApiVehicleIncludedContractOption

--- types/admin/vehicle/vehicle.d.ts
+++ types/admin/vehicle/vehicle.d.ts
@@ -1,6 +1,7 @@
 import { ContractState } from './../../index'
 import { PaymentGateway } from './../../payment'
 import { PriceSpecification } from './../../priceSpecification'
+import { ITermsOfServiceResponse } from './../../termsOfService'
 
 export interface IApiVehicleWithContracts {
   vehicleId: number
@@ -35,4 +36,5 @@ export interface IApiVehicleContract {
 export interface IApiVehicleIncludedContractOption {
   description: string
   assoc: 'PROPERTY' | 'ADDITIONAL'
+  termsOfService: ITermsOfServiceResponse | null
 }
