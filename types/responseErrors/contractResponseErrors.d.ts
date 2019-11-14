bd46714408224181e55519b2f2e7ec2fb04f71ac
fragus-kurt <54097185+fragus-kurt@users.noreply.github.com>
2019-11-14 17:05:09 +0100

    Added some new error types

--- types/responseErrors/contractResponseErrors.d.ts
+++ types/responseErrors/contractResponseErrors.d.ts
@@ -1,4 +1,16 @@
+
+export type ContractResponseErrors =
+  | ContractUpdateErrors
+  | PostContractResponseErrors
+  | WarrantyCheckErrors
+
+
 type ContractUpdateErrors = 'CONTRACT_TRANSITION_INVALID_OR_ILLEGAL'
 type PostContractResponseErrors = 'CONTRACT_MISSING_OR_INVALID_INFORMATION'
 
-export type ContractResponseErrors = ContractUpdateErrors | PostContractResponseErrors
+type WarrantyCheckErrors =
+  | 'WARRANTY_FREE_CONTRACT_NOT_ALLOWED'
+  | 'WARRANTY_TEMPLATE_ID_NOT_FOUND'
+  | 'WARRANTY_START_MILAGE_MISSING'
+  | 'WARRANTY_START_MILAGE_EXCEEDS_WARRANTY_MAX_MILAGE'
+  | 'WARRANTY_END_MILAGE_EXCEEDS_WARRANTY_MAX_MILAGE'
