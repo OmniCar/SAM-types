e354c1b0cf14b0f60aa376630b36a0c1d201ebc0
Marko K. S <marko.seppanen@gmail.com>
2020-04-15 14:34:36 +0200

    Revamp to enum

--- types/fuelType.d.ts
+++ types/fuelType.d.ts
@@ -1,8 +1,10 @@
 // Normalized fuel type names.
-export const FT_PETROL = 'Petrol'
-export const FT_DIESEL = 'Diesel'
-export const FT_ELECTRIC = 'Electric'
-export const FT_HYBRID = 'Hybrid' // Note: Not a fuel type per se, but more of a clarification for customer. Requested by stakeholder.
+export enum FuelTypes {
+  PETROL = 'Petrol',
+  DIESEL = 'Diesel',
+  ELECTRIC = 'Electric',
+  HYBRID = 'Hybrid' // Note: Not a fuel type per se, but more of a clarification for customer. Requested by stakeholder.
+}
 
 export interface FuelType {
   name: string
