fbaba61591e593421ba75e16b37cd975925624e7
Marko K. Seppänen <56867193+FragusMarko@users.noreply.github.com>
2020-04-15 15:19:13 +0200

    Feature/fuel type names (#265)
    * Rollback
    
    Co-authored-by: Marko K. S <marko.seppanen@gmail.com>

--- types/fuelType.d.ts
+++ types/fuelType.d.ts
@@ -1,11 +1,3 @@
-// Normalized fuel type names.
-export enum FuelTypes {
-  PETROL = 'Petrol',
-  DIESEL = 'Diesel',
-  ELECTRIC = 'Electric',
-  HYBRID = 'Hybrid' // Note: Not a fuel type per se, but more of a clarification for customer. Requested by stakeholder.
-}
-
 export interface FuelType {
   name: string
   id?: number
