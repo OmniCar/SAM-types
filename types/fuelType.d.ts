0e3b440620a88432eb7ee5ecaa4867de39748ef0
Marko K. Seppänen <56867193+FragusMarko@users.noreply.github.com>
2020-04-15 11:37:35 +0200

    Add normalized fuel type names (#263)
    Co-authored-by: Marko K. S <marko.seppanen@gmail.com>

--- types/fuelType.d.ts
+++ types/fuelType.d.ts
@@ -1,3 +1,9 @@
+// Normalized fuel type names.
+export const FT_PETROL = 'Petrol'
+export const FT_DIESEL = 'Diesel'
+export const FT_ELECTRIC = 'Electric'
+export const FT_HYBRID = 'Hybrid' // Note: Not a fuel type per se, but more of a clarification for customer. Requested by stakeholder.
+
 export interface FuelType {
   name: string
   id?: number
