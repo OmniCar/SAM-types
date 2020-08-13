f28f725db60cc17bbab0677ea8d4d16857078f83
olexandr <olexandr.drahanchuk@gmail.com>
2020-08-13 20:02:29 +0300

    add interface for user roles list

--- types/admin/user/userList.d.ts
+++ types/admin/user/userList.d.ts
@@ -2,11 +2,17 @@ import { UserRole } from '../auth'
 
 export type UserListOrderByType = 'name' | 'role' | 'email' | 'phone'
 
+interface INullableRole {
+  providerId: number
+  role: UserRole | null
+}
+
 export interface IUserListRecord {
   userId?: number
   name: string
   email: string
   phone: string
-  role: UserRole
+  role?: UserRole
+  roles?:  INullableRole[]
   localeCode?: string
 }
