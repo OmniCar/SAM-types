522b563dbb1a4cb3d0c4b76054a3bc90f129b479
prokhorenkovatv <prokhorenkovatv@gmail.com>
2020-12-29 09:30:05 +0200

    Add calculateBalance contract types

---
+++ types/admin/contract/calculateBalance.d.ts
@@ -0,0 +1,14 @@
+export interface ISettlementInvoices {
+  regularPayments: number
+  cashPayments: number
+  providerPayments: number
+}
+
+export interface ICalculateBalanceResponse {
+  workshopBalance: number
+  paymentBalance: number
+  workshopCosts: number
+  invoicesTotalSum: number
+  settlementInvoices: ISettlementInvoices
+  providerTotalCost: number
+}
