499b91ae63cebd5750d9312d4747415a4082925d
Marko Seppänen <marko.seppanen@gmail.com>
2021-05-17 13:19:08 +0200

    Add

--- types/index.ts
+++ types/index.ts
@@ -53,6 +53,8 @@ export enum ContractActionType {
   sendOfferReminderMail3 = 154, // A reminder about offer was sent to the customer
   sendContractSuspendedMail = 155, // A suspended mail was sent to the customer
   sendContractOfferMail = 156, // A contract offer was sent to the customer
+  resendContractOfferMail = 1565, // Resent offer to the customer via "Resend Offer" action.
+  sendOfferToOneselfMail = 1566, // End-user sent the offer to oneself (via Webcalc).
   sendContractReactivationMail = 157, // A contract reactivation mail was sent to the customer
   sendContractAdjustmentMail = 158, // A contract adjustment mail was sent to the customer
   sendContractSettlementMail = 159, // A contract settlement mail was sent to the customer
