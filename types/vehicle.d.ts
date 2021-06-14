ac03c3a1984732627a42b3b3b903813c7f40c071
Marko K. Seppänen <56867193+FragusMarko@users.noreply.github.com>
2021-06-14 17:07:09 +0200

    Feature - Add engine power and vehicle type for vehicle and vehicle lookups (#341)
    * Add params enginePower and vehicleType
    
    * Fix
    
    Co-authored-by: Marko Seppänen <marko.seppanen@gmail.com>

--- types/vehicle.d.ts
+++ types/vehicle.d.ts
@@ -11,7 +11,26 @@ export interface Vehicle {
   brand: Brand
   model: Model
   fuelType: FuelType
-  contracts?: VehicleContracts
   modelYear: number
+  engineMaxPower?: number // Maximum power in kW, integer or decimal number like 132 or 115.5.
+  vehicleType?: TVehicleType
+  contracts?: VehicleContracts
   hasActiveWarranty?: boolean
 }
+
+export type TVehicleType =
+  | '' // Unknown vehicle type.
+  | 'Trailer' // sv: Släp.
+  | 'Bus'
+  | 'Truck' // sv: Lastbil.
+  | 'Special vehicle'
+  | 'Van' // sv: Skåpbil/Paketbil/Varubil.
+  | 'Car'
+  | 'Tractor'
+  | 'Mobile machinery'
+  | 'Off-road vehicle'
+  | 'Motorcycle'
+  | 'L5' // Motor-vehicle with three wheels, sv: Trehjuligt motorfordon.
+  | 'Moped'
+  | 'Forklift' // sv: Truck.
+  | 'Other'
