fbbbef5a6df55be6fba1e633bc71f7dedaa5b70c
Marko K. Seppänen <56867193+FragusMarko@users.noreply.github.com>
2021-10-05 13:59:11 +0200

    Add a few types needed for fi-warranty for RC 5 (#353)
    Co-authored-by: Marko Seppänen <marko.seppanen@gmail.com>

--- types/country.d.ts
+++ types/country.d.ts
@@ -1,3 +1,10 @@
+/**
+ * ISO 3166-1 two letter (Alpha-2) codes for all supported countries.
+ *
+ * @link https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes
+ */
+export type IsoCountryCodeSupported = 'DK' | 'SE' | 'FI' // NOTE: Currently 100% supported countries.
+
 export interface ICountryRequest {
   isoName: string
 }
