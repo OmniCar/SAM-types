06b83c1a96bfce85e898c2c2d4155ab124b37fd1
Linnea Smedberg <linnea@bilgaranti.local>
2022-01-27 14:11:12 +0100

    Added outage types

---
+++ types/outage.d.ts
@@ -0,0 +1,8 @@
+export enum OutageSeverityLevel {
+    Low,
+    Medium,
+    High,
+}
+
+// Represents the actions that can be disabled
+export type Action = 'Reports' | 'ServiceContracts' | 'ContractCreation'
