19f94efa1b46b306ed6ad6458fbd065843c05665
Linnea Smedberg <linnea@bilgaranti.local>
2022-03-14 14:16:37 +0100

    Added visibleToOtherChildren prop to IContractProviderRequest and Response

--- types/contractProvider.d.ts
+++ types/contractProvider.d.ts
@@ -19,6 +19,7 @@ export interface IContractProviderRequest {
   providerIdentifier: string
   contactPerson: IContactPersonRequest
   termsOfTradeRef: string
+  visibleToOtherChildren: boolean
   webCalcEnabled: boolean
   webTitle: string | null
   isUseWhiteBGOnWeb: boolean
@@ -27,6 +28,7 @@ export interface IContractProviderRequest {
   webLogoSquare: string | null
   freeWarrantiesButton: boolean
   disableContractTemplates: boolean
+  productsEnabled: boolean
   parentProviderId: number | null
   warrantyOnboardingConfig: WarrantyOnboardingConfig | null
 }
@@ -50,6 +52,7 @@ export interface IContractProviderResponse {
   providerIdentifier: string
   contactPerson: IContactPersonResponse
   termsOfTradeRef: string
+  visibleToOtherChildren: boolean
   webCalcEnabled: boolean
   webTitle: string | null
   isUseWhiteBGOnWeb: boolean
