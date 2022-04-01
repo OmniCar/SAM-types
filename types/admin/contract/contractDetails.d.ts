6997957b1e7f04fc204064ae5e887cb775983d42
olexandr <olexandr.drahanchuk@gmail.com>
2022-04-01 15:08:09 +0300

    changed contractDetails type, rename settledMileage and added charges for other value types

--- types/admin/contract/contractDetails.d.ts
+++ types/admin/contract/contractDetails.d.ts
@@ -48,10 +48,14 @@ export interface IContractDetailsRecord {
   adjustedFrom?: string
   createdByProvider?: string
   provider?: string
-  settledMileage?: number | null
+  settledUnits?: number | null
   overdrivenCharge?: number
   underdrivenCharge?: number
   calculationMethod?: number
+  overHoursCharge?: number
+  underHoursCharge?: number
+  overServicesCharge?: number
+  underServicesCharge?: number
 }
 
 export interface IAdminCustomTermsRecord {
