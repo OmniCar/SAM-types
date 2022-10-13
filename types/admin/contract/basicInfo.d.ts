67469237583e41df589af63e11904d7d9db638dd
Linnea Smedberg <linnea@bilgaranti.local>
2022-10-13 09:19:01 +0200

    Solved merge conflict

--- types/admin/contract/basicInfo.d.ts
+++ types/admin/contract/basicInfo.d.ts
@@ -13,9 +13,5 @@ export interface IBasicContractInfo {
   productCanAddWarranty: boolean
   productHasWarranty: boolean
   productContract: boolean
-<<<<<<< HEAD
   offerExpirationDate?: string
-=======
-  offerExpirationDate?: Date
->>>>>>> 062f96d3d9a60abb6f53b8b9723cd3e7e8ff2414
 }
