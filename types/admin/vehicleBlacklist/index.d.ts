66f527de95775fefb628e5d9b0cde6d05679df82
Linnea Smedberg <linnea@bilgaranti.local>
2022-11-22 11:45:56 +0100

    Added seperate request and response types for vehicle blacklist

--- types/admin/vehicleBlacklist/index.d.ts
+++ types/admin/vehicleBlacklist/index.d.ts
@@ -1,8 +1,15 @@
-export interface IBlacklistedVehicle {
+export interface IBlacklistedVehicleRequest {
     id?: number
     brand: string
     model: string
     fuelType: string
 }
 
-export type BlacklistedVehicleOrderByType = keyof IBlacklistedVehicle
+export interface IBlacklistedVehicleResponse {
+    id: number
+    brand: string
+    model: string
+    fuelType: string
+}
+
+export type BlacklistedVehicleOrderByType = keyof IBlacklistedVehicleRequest
