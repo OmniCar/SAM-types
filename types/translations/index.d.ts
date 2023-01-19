845a9c2e441bfee304c7097d1ee3602d938f1f53
Oleksandr Drahanchuk <olexandr.drahanchuk@gmail.com>
2023-01-19 17:47:52 +0200

    change types for translation_applications

--- types/translations/index.d.ts
+++ types/translations/index.d.ts
@@ -1,6 +1,6 @@
 import { IsoLocale } from '../locale'
 
-export type TTranslationApplication = 'system' | 'admin' | 'frontend' | 'emailtemplates'
+export type TTranslationApplication = 'API' | 'Admin' | 'WebCalc' | 'EmailTemplates'
 
 export interface ITranslationValueRequest {
   locale: IsoLocale
