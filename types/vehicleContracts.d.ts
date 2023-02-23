1767d78eb1dd1785f5c1cc400c0ed36814b5424a
Marko Seppänen <marko.seppanen@gmail.com>
2023-02-23 13:06:10 +0100

    WIP - Tightening of types

--- types/vehicleContracts.d.ts
+++ types/vehicleContracts.d.ts
@@ -2,8 +2,23 @@ import { IContractTemplateResponse } from './contractTemplate'
 import { DurationOptions } from './durationOptions'
 
 // **TODO: Rename to Product Contracts
+// export interface VehicleContracts {
+//   contracts: IContractTemplateResponse[]
+//   durations: DurationOptions[] | null // Structure for ContractFlowDurationMileageVer1 for contracts using old legacy pricing.
+//   durationMap: any // Structure for ContractFlowDurationMileageVer2 for V4PricingTool contracts.
+// }
+
 export interface VehicleContracts {
   contracts: IContractTemplateResponse[]
   durations: DurationOptions[] | null // Structure for ContractFlowDurationMileageVer1 for contracts using old legacy pricing.
-  durationMap: any // Structure for ContractFlowDurationMileageVer2 for V4PricingTool contracts.
+  mileageDurationsMap: TMileageDurations // Structure for ContractFlowDurationMileageVer2 for V4PricingTool contracts.
+}
+
+export type TMileageDurations = Map<string, IMileageDurations>
+
+export interface IMileageDurations {
+  options: {
+    mileage: number // Eg. 60000. In km.
+    months: number[] // Eg. [24,36,48,60].
+  }[]
 }
