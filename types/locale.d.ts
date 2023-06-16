9f7b6e72599a89fc990fab3e1f452e692d49eb27
Marko Seppänen <marko.seppanen@gmail.com>
2023-06-16 13:39:04 +0200

    Added en

--- types/locale.d.ts
+++ types/locale.d.ts
@@ -8,4 +8,16 @@ export interface Locale {
   vatPct: number
 }
 
-export type IsoLocale = 'da-DK' | 'sv-SE' | 'fi-FI' | 'nb-NO' | 'en-GB'
+/**
+ * "
+ * For example, to specify common English, use the 'en' language designator as
+ * locale. To specify the English language as it is used in the United
+ * Kingdom, use 'en-GB' as the locale.
+ * "
+ *
+ * Reference:
+ * https://lingohub.com/developers/supported-locales/language-designators-with-regions
+ *
+ * @note 'en-GB' is deprecated, please use 'en' for common English instead if possible.
+ */
+export type IsoLocale = 'da-DK' | 'sv-SE' | 'fi-FI' | 'nb-NO' | 'en' | 'en-GB' // NOTE: 'en-GB' is deprecated.
