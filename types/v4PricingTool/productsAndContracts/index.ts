687811b388e7e8626cbd25f57664494a6911effa
Marko Seppänen <marko.seppanen@gmail.com>
2024-04-15 15:30:41 +0200

    Deleted depredated payment types + added new product type

--- types/v4PricingTool/productsAndContracts/index.ts
+++ types/v4PricingTool/productsAndContracts/index.ts
@@ -1,10 +1,10 @@
-export type TV4PTProductType = 'ServiceAgreement' | 'Warranty' | 'WarrantyAndServiceAgreement'
+export type TV4PTProductType = 'ServiceAgreement' | 'Warranty' | 'WarrantyAndServiceAgreement' | 'Vary' // Some other product type in V4.
 
 // V4PricingTool Payment Type.
 export type TV4PTPaymentType =
   | 'DealerPaid' // One-time dealer payment.
   | 'CustomerSubscription' // Customer monthly subscription.
-  | 'CustomerPaymentCard' // Deprecated, use 'CustomerSubscription' instead. /2023-01-01
-  | 'Subscription' // Deprecated, use 'CustomerSubscription' instead. /2023-01-01
-  | 'DealerPaidMonthly' // Deprecated, use 'DealerPaid' instead. /2023-01-01
-  | 'OneTimeDealerPayment' // Deprecated, use 'DealerPaid' instead. /2023-01-01
+// | 'CustomerPaymentCard' // Deprecated, use 'CustomerSubscription' instead. /2023-01-01
+// | 'Subscription' // Deprecated, use 'CustomerSubscription' instead. /2023-01-01
+// | 'DealerPaidMonthly' // Deprecated, use 'DealerPaid' instead. /2023-01-01
+// | 'OneTimeDealerPayment' // Deprecated, use 'DealerPaid' instead. /2023-01-01
